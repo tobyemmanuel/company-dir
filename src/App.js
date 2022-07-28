753c680d282410175f993a8734631773bd587b91
tobyemmanuel <tobyemmanuel7@gmail.com>
2022-07-28 11:26:09 +0100

    Created files for the web components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+//import components
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import AboutSection from './components/AboutSection';
+import TableSection from './components/TableSection';
+import MapSection from './components/MapSection';
+import Footer from './components/Footer';
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <HeroSection/>
+      <AboutSection/>
+      <TableSection/>
+      <MapSection/>
+      <Footer/>
     </div>
   );
 }

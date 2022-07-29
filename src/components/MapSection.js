f61cd7e37572184514345ba08d7e15445684ae93
tobyemmanuel <tobyemmanuel7@gmail.com>
2022-07-29 10:34:19 +0100

    Added map section

--- src/components/MapSection.js
+++ src/components/MapSection.js
@@ -2,6 +2,21 @@ import React from 'react'
 
 export default function MapSection() {
   return (
-    <div>mapSection</div>
+    <div>
+      <section id="map-section" className="low-padding">
+        <div className="container" data-aos="fade-up">
+
+          <div className="section-header">
+            <h2>View the companies on the map</h2>
+            <p>Company <span>Location</span></p>
+          </div>
+
+          <div className="map mb-3">
+            MAP HERE
+          </div>
+
+        </div>
+      </section>
+    </div>
   )
 }

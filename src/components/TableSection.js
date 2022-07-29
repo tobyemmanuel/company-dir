190b396ae1d67bc065724cbb26ad2aa5760dc258
tobyemmanuel <tobyemmanuel7@gmail.com>
2022-07-29 10:31:12 +0100

    Added a section (table) for company listing

--- src/components/TableSection.js
+++ src/components/TableSection.js
@@ -2,6 +2,23 @@ import React from 'react'
 
 export default function TableSection() {
   return (
-    <div>tableSection</div>
+    <div>
+      <section id="table-section" className="low-padding section-bg">
+        <div className="container" data-aos="fade-up">
+
+          <div className="section-header">
+            <h2>View the companies</h2>
+            <p>Company <span>Listing</span></p>
+          </div>
+
+          <div className="row gy-4">
+            <div className="col-lg-12">
+                TABLE HERE
+            </div>
+          </div>
+
+        </div>
+      </section>
+    </div>
   )
 }

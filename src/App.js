409888d0767299ebf01a4ac4a6e4638e91df20c2
tobyemmanuel <tobyemmanuel7@gmail.com>
2022-07-29 09:44:09 +0100

    Added the Header Section, stylesheet, and logo; Imported image assests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/styles.css'
 
 //import components
 import Header from './components/Header';
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import logo from '../img/logo.png';
 
 export default function Header() {
   return (
-    <div>header</div>
+    <div>
+  <header id="header" className="header fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+      <a href="#home" className="logo d-flex align-items-center me-auto me-lg-0">
+        <img src={logo} alt="logo"/>
+      </a>
+
+      <nav id="navbar" className="navbar">
+        <ul>
+          <li><a href="#hero">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#directory">Directory</a></li>
+        </ul>
+      </nav>
+
+      <a className="btn-contact" href="Contact">Contact</a>
+      <i className="nav-toggle nav-show bi bi-list"></i>
+      <i className="nav-toggle nav-hide d-none bi bi-x"></i>
+    </div>
+  </header> 
+    </div>
   )
 }
